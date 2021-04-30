8292070ba1e95b9e47d44e658a79c4100b6e8069
alaaloaa <alaaa2083@gmail.com>
2021-04-30 23:54:48 +0200

    first commit

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -94,7 +94,7 @@ class JobFactory extends Factory
             'companyName' => $this->faker->company,
             'aboutCompany' => $this->faker->text(500),
             'salary' => $this->faker->numberBetween($min = 100, $max = 3000),
-            'logo' => $this->faker->image('public', 400, 300, 'Company Logo'),
+            'logo' => $this->faker->image('public/storage/images/fake/jobs', 400, 300, 'Company Logo'),
             'status' => $this->faker->numberBetween($min = 0, $max = 1),
             'created_at' => $this->faker->dateTimeBetween($startDate = '-30 days', $endDate = 'now'),
         ];
