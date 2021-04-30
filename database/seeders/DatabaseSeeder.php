a425195fdf9f052fc42599587d49946ab73cdef0
alaaloaa <alaaa2083@gmail.com>
2021-05-01 01:16:24 +0200

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(SkillsTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
+        // $this->call(SkillsTableSeeder::class);
+        // $this->call(CountriesTableSeeder::class);
         \App\Models\User::factory(50)->create();
         // \App\Models\Job::factory(50)->create();
         // \App\Models\Job::factory(40)->create();
