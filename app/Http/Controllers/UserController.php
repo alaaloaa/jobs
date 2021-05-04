2bd83cea2d023b18139da736b0824f8b3deee279
alaaloaa <alaaa2083@gmail.com>
2021-05-04 03:18:31 +0200

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
 
             //     // unlink($path); // delete old pic
             // }
-            Storage::disk('s3')->put('images/users', $request->file('avatar'));
+            Storage::disk('s3')->put('avatars/1', $request->file('avatar'));
 
             // $request->file('avatar')->store('avatars', 's3');
             // $user->avatar = Storage::disk('s3')->url($path);
